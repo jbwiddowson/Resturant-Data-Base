33b05951e2ff4d7e4db41e6c0c5c8a2ee29038eb
Jacob <158038235+jbwiddowson@users.noreply.github.com>
2025-09-19 02:53:53 -0400

    Initial Setup and Database Creation.sh

---
+++ Initial Setup and Database Creation.sh
@@ -0,0 +1,11 @@
+# Connect to MySQL
+sudo mysql -u root -p
+
+# Or create and use specific user
+sudo mysql -u root -p -e "
+CREATE USER 'restaurant_admin'@'localhost' IDENTIFIED BY 'RestaurantPass123!';
+GRANT ALL PRIVILEGES ON restaurant_db.* TO 'restaurant_admin'@'localhost';
+FLUSH PRIVILEGES;"
+
+# Connect as restaurant admin
+mysql -u restaurant_admin -p
